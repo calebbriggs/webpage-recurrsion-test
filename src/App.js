c33c08eec59b08b5fcdfa66c8f668e59c1334d7e
caleb briggs <caleb.briggs@gmail.com>
2019-09-25 10:29:47 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   const [html, setHtml] = useState('<h1>...Loading Content</h1>');
   async function fetchgoogle() {
-    var res = await fetch('http://localhost:3010?page=https://www.lipsum.com/');
-    var page = await res.text();
+    const res = await fetch('http://localhost:3010?page=https://www.lipsum.com/');
+    const page = await res.text();
     setHtml(page);
   }
   useEffect(() => {
