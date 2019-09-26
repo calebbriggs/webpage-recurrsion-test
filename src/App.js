d2b066a6231fb1a10746b73518e1fa8c5a0781fd
caleb briggs <caleb.briggs@gmail.com>
2019-09-26 15:43:53 -0500

    adding page call back in

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 function App() {
   const [html, setHtml] = useState('<h1>...Loading Content</h1>');
   async function fetchgoogle() {
-    // const res = await fetch('http://localhost:3010?page=https://www.lipsum.com/');
-    // const page = await res.text();
-    // setHtml(page);
+    const res = await fetch('http://localhost:3010?page=https://www.google.com');
+    const page = await res.text();
+    setHtml(page);
   }
   useEffect(() => {
     fetchgoogle();
@@ -14,6 +14,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Recurrsion script</p>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
       </header>
     </div>
   );
